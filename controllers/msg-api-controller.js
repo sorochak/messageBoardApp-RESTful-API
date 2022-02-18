e47aa990a76e5bf5f1744d0a99164aa395ff87f0
AustenSorochak <austensorochak@gmail.com>
2022-02-17 22:13:49 -0800

    added messages array to msg-api-controller.js

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -7,5 +7,38 @@ const getAllMessages = (req, res) => {
   const addNewMessage = async (req, res) => {
     res.status(200).send('Successful API POST Request');
   };
-  
+
+  const messages = [
+    {
+        id: 0,
+        name: "Bill",
+        msgText: "I don't know. Philosophize with him!",
+    },
+    {
+        id: 1,
+        name: "Ted",
+        msgText: "All we are is dust in the wind, dude.",
+    },
+    {
+        id: 2,
+        name: "Soldier",
+        msgText: "Are you suggesting coconuts migrate?",
+    },
+    {
+        id: 3,
+        name: "The Black Knight",
+        msgText: "Just a flesh wound.",
+    },
+    {
+        id: 4,
+        name: "God",
+        msgText: "Course it's a good idea!",
+    },
+    {
+        id: 5,
+        name: "Hunter S. Thompson",
+        msgText: "Too weird to live, too rare to die!",
+    },
+];
+
   export { getAllMessages, addNewMessage };
