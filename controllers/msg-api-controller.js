543f6defb3164e7267ebe8ee1cbdeecb4f6724ad
AustenSorochak <austensorochak@gmail.com>
2022-02-25 14:15:14 -0800

    modify getAllMessages() API handler to use the database instrad of in-memory Array

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,8 +1,12 @@
-import messageSchema from '../models/message-schema.js';
+//import messageSchema from '../models/message-schema.js';
+import mongoose from 'mongoose';
+
+const messageModel = mongoose.model('message');
 
 // GET Request Handler
-const getAllMessages = (req, res) => {
+const getAllMessages = async (req, res) => {
     try{
+        let messages = await messageModel.find( {}, '', { sort: { _id: -1 } } ).exec();
         res.status(200).json(messages);
     } catch(err) {
         res.status(400).send('Bad Request');
@@ -32,37 +36,37 @@ const getAllMessages = (req, res) => {
     //res.status(200).send('Successful API POST Request');
   };
 
-  const messages = [
-    {
-        id: 0,
-        name: "Bill",
-        msgText: "I don't know. Philosophize with him!",
-    },
-    {
-        id: 1,
-        name: "Ted",
-        msgText: "All we are is dust in the wind, dude.",
-    },
-    {
-        id: 2,
-        name: "Soldier",
-        msgText: "Are you suggesting coconuts migrate?",
-    },
-    {
-        id: 3,
-        name: "The Black Knight",
-        msgText: "Just a flesh wound.",
-    },
-    {
-        id: 4,
-        name: "God",
-        msgText: "Course it's a good idea!",
-    },
-    {
-        id: 5,
-        name: "Hunter S. Thompson",
-        msgText: "Too weird to live, too rare to die!",
-    },
-];
+//   const messages = [
+//     {
+//         id: 0,
+//         name: "Bill",
+//         msgText: "I don't know. Philosophize with him!",
+//     },
+//     {
+//         id: 1,
+//         name: "Ted",
+//         msgText: "All we are is dust in the wind, dude.",
+//     },
+//     {
+//         id: 2,
+//         name: "Soldier",
+//         msgText: "Are you suggesting coconuts migrate?",
+//     },
+//     {
+//         id: 3,
+//         name: "The Black Knight",
+//         msgText: "Just a flesh wound.",
+//     },
+//     {
+//         id: 4,
+//         name: "God",
+//         msgText: "Course it's a good idea!",
+//     },
+//     {
+//         id: 5,
+//         name: "Hunter S. Thompson",
+//         msgText: "Too weird to live, too rare to die!",
+//     },
+// ];
 
   export { getAllMessages, addNewMessage };
