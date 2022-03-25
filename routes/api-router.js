b1e45d86d2ecec84fa09f4799bbf5b028705f0bd
AustenSorochak <austensorochak@gmail.com>
2022-03-24 19:14:26 -0700

    protect post request for /messages endpoint

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
 import { getAllLabs } from '../controllers/exam-api-controller.js';
 import { registerNewUser } from '../controllers/user-api-controller.js';
@@ -7,7 +8,7 @@ const router = express.Router();
 
 router.route('/messages')
 .get(getAllMessages)
-.post(addNewMessage);
+.post(passport.authenticate('basic', { session: false }), addNewMessage);
 
 router.route('/users')
 .post(registerNewUser);
